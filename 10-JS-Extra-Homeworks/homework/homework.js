e5fd965f22d7958bf90c0239fdd5d5337178412d
Akamasoa <fsgenre@gmail.com>
2021-06-04 00:16:55 -0300

    css

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -26,6 +26,20 @@ function capToFront(s) {
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
   //Escribe tu código aquí
+  var arrayMayusculas = [];
+  var arrayMinusculas = [];
+
+  for(var i = 0; i < s.length; i++){
+    if(s[i] === s[i].toLowerCase()){
+      arrayMinusculas.push(s[i]);
+    }
+    if(s[i] === s[i].toUpperCase()){
+      arrayMayusculas.push(s[i]);
+    }
+  }
+  var stringMayusculas = arrayMayusculas.join('');
+  var stringMinusculas = arrayMinusculas.join('');
+  return stringMayusculas + stringMinusculas;
 }
 
 
@@ -35,7 +49,7 @@ function asAmirror(str) {
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
-} 
+  
 
 
 function capicua(numero){
@@ -57,6 +71,16 @@ function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
+  arr.sort(function(a,b){
+    if(a.length>b.length){
+      return 1;
+    }
+    if(a.length<b.length){
+      return -1;
+    }
+    return 0;
+  })
+  return arr;
 }
 
 
